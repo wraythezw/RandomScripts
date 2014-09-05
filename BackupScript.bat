15f31ab1bc1c133f2ca2b0f2f4162f84e591ff07
RakeZim <johannes.swart1@gmail.com>
2014-09-05 11:25:31 +0200

    Update BackupScript.bat

--- BackupScript.bat
+++ BackupScript.bat
@@ -5,14 +5,17 @@ echo *              MIT License                  *
 echo *         keith.rose.zw@gmail.com           *
 echo *********************************************
 
-set folder=z:\backups\%date:/=_%_backup
+set Source= "C:\Data"
+set folder= "D:\Backup"
+
 if exist %folder% (
  echo Backup file %folder%.rar already exists. Did you already run the backup?
 ) else (
  echo No previous backup. Backing up.
  echo Starting backup. Do not close this window!!!!
  echo ........
- rar a -r %folder%.rar C:\Office1 > %folder%_log.txt
+
+rar a -r %folder%.rar %Source% >> %folder%_log.txt | tail -f %folder%_log.txt
 )
 echo Backup completed succesfully. Press any key to continue.
 pause
